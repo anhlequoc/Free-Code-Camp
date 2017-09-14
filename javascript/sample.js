84dc25f29186fc123a0a4776bdabe9ad155dbdcb
Anh Le <lequocanh88@gmail.com>
2017-09-14 18:52:45 +0700

    ...;

--- javascript/sample.js
+++ javascript/sample.js
@@ -1,8 +1,19 @@
-function chunkArrayInGroups(arr, size) {
-    // Break it up.
-    var result = [];
-    var childArr = [];
- 
-}
+function bouncer(arr) {
+    // Don't show a false ID to this bouncer.
+    arr = arr.filter(function(e) {
+        if (e === false || e === null || e === 0 || e === "" || e === "" || e === undefined) {
+            return false;
+        } else if (isNaN(e)) {
+            return false;
+        }        
+        return true;
+    });
+    
+    console.log(arr);  
+    return arr;
+  }
+  bouncer([7, "ate", "", false, 9]);
+  bouncer(["a", "b", "c"])
+  bouncer([false, null, 0, NaN, undefined, ""]);
+
   
-chunkArrayInGroups(["a", "b", "c", "d"], 2);
