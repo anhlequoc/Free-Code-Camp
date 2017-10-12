4c0710c699e7fdb0db060b2548a419f9add7b254
Anh Le <lequocanh88@gmail.com>
2017-10-13 00:01:54 +0700

    working...

--- Random Quote Machine/quote.js
+++ Random Quote Machine/quote.js
@@ -1,5 +1,15 @@
 function getQuote() {
-   console.log('fine');
+    $.ajax( {
+        url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+        success: function(data) {
+            var post = data.shift(); // The data is an array of posts. Grab the first one.
+            console.log(post);
+        },
+        cache: false
+      });
+    // $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+    //     $("body").append(a[0].content + "<p>— " + a[0].title + "</p>")
+    // });
 }
 
 $(document).ready(function() {
