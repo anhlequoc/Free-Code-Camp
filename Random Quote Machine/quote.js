0f38f33f56e719df0caab61e2b73acdc91417f0c
Anh Le Quoc <anhlequoc88@Anhs-MacBook-Pro.local>
2017-11-02 01:36:18 +0700

    modify...

--- Random Quote Machine/quote.js
+++ Random Quote Machine/quote.js
@@ -1,15 +1,17 @@
 function getQuote() {
     $.ajax( {
+        // beforeSend: function(request) {
+        //     request.setRequestHeader("Access-Control-Allow-Origin", "http://null");
+        // }, 
+        headers: {
+            "Access-Control-Allow-Origin": "http://null"
+        },
         url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
         success: function(data) {
             var post = data.shift(); // The data is an array of posts. Grab the first one.
             console.log(post);
-        },
-        cache: false
+        }
       });
-    // $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-    //     $("body").append(a[0].content + "<p>— " + a[0].title + "</p>")
-    // });
 }
 
 $(document).ready(function() {
